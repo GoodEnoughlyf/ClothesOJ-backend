88220aa3de864dd2f303426bec525e54850e6231
liyifu <1516883523@qq.com>
2023-11-22 00:59:44 +0800

    完成了User、Question、QuestionSubmit的接口实现

--- src/main/java/com/liyifu/clothesojbackend/service/impl/QuestionSubmitServiceImpl.java
+++ src/main/java/com/liyifu/clothesojbackend/service/impl/QuestionSubmitServiceImpl.java
@@ -17,28 +17,35 @@ import com.liyifu.clothesojbackend.model.enums.QuestionSubmitStatusEnum;
 import com.liyifu.clothesojbackend.model.vo.QuestionSubmitVO;
 import com.liyifu.clothesojbackend.service.QuestionService;
 import com.liyifu.clothesojbackend.service.QuestionSubmitService;
+import com.liyifu.clothesojbackend.service.UserService;
 import com.liyifu.clothesojbackend.utils.SqlUtils;
 import org.apache.commons.lang3.ObjectUtils;
 import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
-* @author liyifu
-* @description 针对表【question_submit(题目提交)】的数据库操作Service实现
-* @createDate 2023-11-14 01:34:16
-*/
+ * @author liyifu
+ * @description 针对表【question_submit(题目提交)】的数据库操作Service实现
+ * @createDate 2023-11-14 01:34:16
+ */
 @Service
 public class QuestionSubmitServiceImpl extends ServiceImpl<QuestionSubmitMapper, QuestionSubmit>
-    implements QuestionSubmitService {
+        implements QuestionSubmitService {
 
     @Resource
     private QuestionService questionService;
 
+    @Resource
+    private UserService userService;
+
     /**
      * 提交题目
+     *
      * @param questionSubmitAddRequest
      * @param loginUser
      * @return
@@ -48,14 +55,14 @@ public class QuestionSubmitServiceImpl extends ServiceImpl<QuestionSubmitMapper,
         //1、检验编程语言是否在题目给出的枚举值中
         String submitLanguage = questionSubmitAddRequest.getSubmitLanguage();
         QuestionSubmitLanguageEnum enumByValue = QuestionSubmitLanguageEnum.getEnumByValue(submitLanguage);
-        if(enumByValue==null){
-            throw new BusinessException(ErrorCode.PARAMS_ERROR,"编程语言错误");
+        if (enumByValue == null) {
+            throw new BusinessException(ErrorCode.PARAMS_ERROR, "编程语言错误");
         }
 
         //2、判断题目是否存在
         Long questionId = questionSubmitAddRequest.getQuestionId();
         Question question = questionService.getById(questionId);
-        if(question==null){
+        if (question == null) {
             throw new BusinessException(ErrorCode.NOT_FOUND_ERROR);
         }
         //3、每个用户串行提交  todo(用限流更加合适 ，需要保证同一时间，用户只能提交成功一次)
@@ -65,12 +72,12 @@ public class QuestionSubmitServiceImpl extends ServiceImpl<QuestionSubmitMapper,
         questionSubmit.setUserId(userId);
         questionSubmit.setSubmitLanguage(submitLanguage);
         questionSubmit.setSubmitCode(questionSubmitAddRequest.getSubmitCode());
-            //设置初始判题状态
+        //设置初始判题状态
         questionSubmit.setSubmitState(QuestionSubmitStatusEnum.WAITING.getValue());
         questionSubmit.setJudgeInfo("{}");
         boolean save = this.save(questionSubmit);
-        if(!save){
-            throw new BusinessException(ErrorCode.SYSTEM_ERROR,"题目提交失败");
+        if (!save) {
+            throw new BusinessException(ErrorCode.SYSTEM_ERROR, "题目提交失败");
         }
         //4、异步执行判题服务  todo (搞懂异步的过程写法）
         Long questionSubmitId = questionSubmit.getId();
@@ -81,13 +88,14 @@ public class QuestionSubmitServiceImpl extends ServiceImpl<QuestionSubmitMapper,
 
     /**
      * 构造条件构造器
+     *
      * @param questionSubmitQueryRequest
      * @return
      */
     @Override
     public QueryWrapper<QuestionSubmit> getQuestionSubmitQueryWrapper(QuestionSubmitQueryRequest questionSubmitQueryRequest) {
-        QueryWrapper<QuestionSubmit> queryWrapper=new QueryWrapper<>();
-        if(questionSubmitQueryRequest==null){
+        QueryWrapper<QuestionSubmit> queryWrapper = new QueryWrapper<>();
+        if (questionSubmitQueryRequest == null) {
             return queryWrapper;
         }
 
@@ -100,35 +108,58 @@ public class QuestionSubmitServiceImpl extends ServiceImpl<QuestionSubmitMapper,
         String sortField = questionSubmitQueryRequest.getSortField();
         String sortOrder = questionSubmitQueryRequest.getSortOrder();
 
-        queryWrapper.eq(StringUtils.isNotBlank(submitLanguage),"submitLanguage",submitLanguage)
-                .eq(ObjectUtils.isNotEmpty(userId),"userId",userId)
-                .eq(ObjectUtils.isNotEmpty(questionId),"questionId",questionId)
-                .eq("isDelete",false)
-                .orderBy(SqlUtils.validSortField(sortField),sortField.equals("ascend"),sortOrder);
+        queryWrapper.eq(StringUtils.isNotBlank(submitLanguage), "submitLanguage", submitLanguage)
+                .eq(ObjectUtils.isNotEmpty(userId), "userId", userId)
+                .eq(ObjectUtils.isNotEmpty(questionId), "questionId", questionId)
+                .eq("isDelete", false)
+                .orderBy(SqlUtils.validSortField(sortField), sortField.equals("ascend"), sortOrder);
 
         return queryWrapper;
     }
 
     /**
      * 获取脱敏后的题目提交信息
+     * 管理员能看到所有人完整的题目提交信息，自己能看到自己提交的题目信息，其他用户只能看到非答案、提交代码的公开信息
+     *
      * @param questionSubmit
      * @param loginUser
      * @return
      */
     @Override
     public QuestionSubmitVO getQuestionSubmitVO(QuestionSubmit questionSubmit, User loginUser) {
-        return null;
+        QuestionSubmitVO questionSubmitVO = QuestionSubmitVO.objToVO(questionSubmit);
+        Long userId = loginUser.getId();
+        if (userId != questionSubmitVO.getUserId() && !userService.isAdmin(loginUser)) {
+            questionSubmitVO.setSubmitCode(null);
+        }
+        return questionSubmitVO;
     }
 
     /**
      * 分页获取脱敏后的题目提交信息
+     *
      * @param questionSubmitPage
      * @param loginUser
      * @return
      */
     @Override
     public Page<QuestionSubmitVO> getQuestionSubmitPage(Page<QuestionSubmit> questionSubmitPage, User loginUser) {
-        return null;
+        //获取Page<QuestionSubmit>中的数据list
+        List<QuestionSubmit> questionSubmitList = questionSubmitPage.getRecords();
+        long current = questionSubmitPage.getCurrent();
+        long pageSize = questionSubmitPage.getSize();
+        long total = questionSubmitPage.getTotal();
+
+        //构建Page<QuestionSubmitVO>
+        Page<QuestionSubmitVO> questionSubmitVOPage = new Page<>(current, pageSize, total);
+
+        //对questionSubmitList进行脱敏
+        List<QuestionSubmitVO> questionSubmitVOList = questionSubmitList.stream().map(
+                        questionSubmit -> getQuestionSubmitVO(questionSubmit, loginUser))
+                .collect(Collectors.toList());
+
+        questionSubmitVOPage.setRecords(questionSubmitVOList);
+        return questionSubmitVOPage;
     }
 }
 
